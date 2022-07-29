9d1a6addafe5eac0ea7fa86a9a15072b470a6baf
unknown <DMITRY.K.V@YANDEX.RU>
2022-07-29 15:07:50 +0300

    file_dz.py

--- file_dz.py
+++ file_dz.py
@@ -1,2 +1,19 @@
-True  age_Dmitry_31
-False  age_Dmitry_100
+
+print(10>9)    #bool
+print (10>11)  #bool
+a= 1           #int
+print(a)
+b= 44.35       #float
+print (b)
+c= 12+1j        #complex
+"привет" "Дмитрий"             #str
+print("привет","Дмитрий")
+e = [1,2,3,4,]
+tpl = (1,2,3,4,)    #tuple
+print (e[2])
+d= {"msk": "495", "spb": "812", "nalchik": "07"}   # dict
+print(d)
+l= [1,100, 333, "listic", 999, "999"] #list
+print(l)
+s= {11,12,11,121212,22222,567,0,11} #set
+print(s, type (s))
